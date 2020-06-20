0bd8a57278707cc4b48adb5331d473007ac1c08d
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-06-20 14:07:02 -0500

    first user story tests finished

--- src/test/java/bookstoread/BookShelf.java
+++ src/test/java/bookstoread/BookShelf.java
@@ -2,6 +2,7 @@ package bookstoread;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BookShelf {
@@ -9,7 +10,7 @@ public class BookShelf {
     private final List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
 
     public void add(String... booksToAdd) {
--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,107 +1,61 @@
 package bookstoread;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("A bookshelf")
 public class BookShelfSpec {
 
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
+
+    /* first feature: As a user, I want to add multiple books to my bookshelf so that I can read them later */
     @Test
     @DisplayName("is empty when no book is added to it")
     public void shelfEmptyWhenNoBookAdded() throws Exception {
-        BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
 
+    /* first feature: As a user, I want to add multiple books to my bookshelf so that I can read them later */
     @Test
     @DisplayName("bookshelf should have two books when two books added")
     void bookshelfContainsTwooBooksWhenTwoBooksAdded(){
-        BookShelf shelf = new BookShelf();
         shelf.add("Effective Java", "Code Complete");
         List<String> books = shelf.books();
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
     }
 
+    /* first feature: As a user, I want to add multiple books to my bookshelf so that I can read them later */
     @Test
+    @DisplayName("when add method is called withoud argument no book will be added")
     public void emptyBookShelfWhenAddAddIsCalledWithoutBooks(){
-        BookShelf shelf = new BookShelf();
         shelf.add();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty");
     }
 
+    /* first feature: As a user, I want to add multiple books to my bookshelf so that I can read them later */
     @Test
+    @DisplayName("Should not be able to add book to books")
     void booksReturnedFromBookShelfIsImmutableForClient(){
-        BookShelf shelf = new BookShelf();
         shelf.add("Effective Java", "Code Complete");
         List<String> books = shelf.books();
-
+        try {
+            books.add("The Mythical Man-Month");
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e){
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException");
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-
-    @Test
-    void nullAssertionTest() {
-        String str = null;
-        assertNull(str);
-        assertNull(str, "str should be null");
-        assertNull(str, () -> "str should be null");
-    }
-
-    @Test
-    void shouldCheckForEvenNumbers() {
-        int number = new Random(10).nextInt();
-        assertTrue(() -> number%2 == 0, number+ " is not an even number.");
-        BiFunction<Integer, Integer, Boolean> divisible = (x, y) -> x % y == 0;
-        Function<Integer, Boolean> multipleOf2 = (x) -> divisible.apply(x, 2);
-        assertTrue(() -> multipleOf2.apply(number), () -> " 2 is not factor of " +
-                number);
-    }
-
-    @Test
-    public void shelfToStringShouldPrintBookCountAndTitles() throws Exception {
-        BookShelf shelf = new BookShelf();
-        List<String> books = shelf.books();
-        shelf.add("The Phoenix Project");
-        shelf.add("Java 8 in Action");
-        String shelfStr = shelf.toString();
-        assertAll( ()  -> assertTrue(shelfStr.contains("The Phoenix Project"),  "1st book title missing"),
-                () -> assertTrue(shelfStr.contains("Java 8 in Action") , "2nd book title missing "),
-                () -> assertTrue(shelfStr.contains("2 books found"),"Book  count missing"));
-
-
-    }
-
-    @Test
-    void thisTestShouldFail() {
-        fail(() -> "This test should fail");
-    }*/
 }
