ebe8b32b6d94aff118713b7356f05f1c7bd88142
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-06-21 14:22:31 -0500

    third user story finished

--- src/main/java/bookstoread/BookShelf.java
+++ src/main/java/bookstoread/BookShelf.java
@@ -1,7 +1,11 @@
 package bookstoread;
 
+import java.time.Year;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class BookShelf {
 
@@ -19,8 +23,17 @@ public class BookShelf {
         return books.stream().sorted().collect(Collectors.toList());
     }
 
-
     public List<Book> arrange(Comparator<Book> criteria) {
         return books.stream().sorted(criteria).collect(Collectors.toList());
+    }
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return groupBy(book -> Year.of(book.getPublishedOn().getYear()));
+    }
+
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books
+                .stream()
+                .collect(groupingBy(fx));
     }
 }
